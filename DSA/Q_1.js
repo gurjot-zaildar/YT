ae09dae0d08f800a877996ba1fdf71ffa0719fb0
gurjot-zaildar <signingames115@gmail.com>
2026-03-30 12:05:32 +0530

    q:-1,2

---
+++ DSA/Q_1.js
@@ -0,0 +1,10 @@
+// Swap two Numbers
+
+let a= 1;
+let b= 2;
+
+let c=a; // c=1
+a=b; // a= 2 ✅
+b=c; // b= 1 ✅
+
+console.log(a,b)
---
+++ DSA/Q_2.js
@@ -0,0 +1,10 @@
+// Swap two numbers without 3rd variable
+
+let a = 10;
+let b= 5;
+
+a= a+b; // 15
+b= a-b; // 10 ✅
+a= a-b; // 5 ✅
+
+console.log(a,b)
