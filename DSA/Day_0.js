1244c729d96bd2f2dea45683cf747a1e72001349
gurjot-zaildar <signingames115@gmail.com>
2026-03-30 10:38:59 +0530

    Day_0

---
+++ DSA/Day_0.js
@@ -0,0 +1,8 @@
+//Q:- Sum of two integer
+//Q:- Greet the User
+
+
+let name = prompt("enter your name");
+let age = Number(prompt("enter your age"))
+
+console.log(`welcome ${name} your age is ${age}`)
